b2ba1ae36a183dbbb0f05efdf38f624c4a22bc41
GBTC406014UR <GBTC406014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-31 11:46:43 -0400

    Added ArrayList method works

--- src/WeekTwoChallenege.java
+++ src/WeekTwoChallenege.java
@@ -8,65 +8,64 @@ public class WeekTwoChallenege {
         //array of 25 burritos
         int[] burritos;
         burritos = new int[25];
-        ArrayList<String> completeBurr = new ArrayList<>();
+        ArrayList<String> burr = new ArrayList<>();
 
-        //array of ingredients
-//        ArrayList<String> rice = new ArrayList<>();
-//        Collections.addAll(rice, "white", "brown", "none", "all");
-        String[] rice= {"white", "brown", "no rice", "all rice"};
-        String[] meat = {"chicken","steak","carnidas","chorizo","sofritas","veggies","no meat","all meat"};
-        String[] beans = {"pinto","black","no beans"};
-        String[] salsa ={"mild","medium","hot","no salsa","all salsa"};
-        String[] veggies = {"lettuce","fajitas veggies","no veggies","all veggies"};
-        boolean cheese = false;
-        boolean guac = false;
-        boolean queso = false;
-        boolean sourcream = false;
+        burr = myBurr();
+        System.out.println(burr);
 
-        boolean cont = true;
-        Random rand = new Random();
-           //int numOfBurr = 25;
-        while(cont) {
-            //fill in complete burrito array
-           // for (int i = 1; i < burritos.length; i++) {//for every burritos starting from 1
-                int randRice = rand.nextInt(rice.length);//random generator for rice
-                String r = (rice[randRice]);
-                completeBurr.add(r);//add to burrito
-
-                //System.out.println(r);
-
-                int randMeat = rand.nextInt(meat.length);//random generator for meat
-                String m = (meat[randMeat]);
-                completeBurr.add(m);
-                //System.out.println(m);
-
-                int randBeans = rand.nextInt(beans.length);//random generator for beans
-                String b = (beans[randBeans]);
-                completeBurr.add(b);
-
-                int randSalsa = rand.nextInt(salsa.length);//random generator for salsa
-                String s = (salsa[randSalsa]);
-                completeBurr.add(s);
-
-                int randVeg = rand.nextInt(veggies.length);//random generator for veggies
-                String v = (veggies[randVeg]);
-                completeBurr.add(v);
-
-                cont = false;
-            System.out.println("After add ingredients: " + completeBurr);
-            //}//end for
-        }//end while
-
-
-
-            //check if the complete burritor contains none or no
+        //check if the complete burritor contains none or no
         // get index of no-rice value and dont calculate it
 
+
     }
-
-public static String myBurr()
+//method to generate different burritos ingredients
+public static ArrayList myBurr()
 {
+    ArrayList<String> completeBurr = new ArrayList<>();
 
+    //array of ingredients
+//        ArrayList<String> rice = new ArrayList<>();
+//        Collections.addAll(rice, "white", "brown", "none", "all");
+    String[] rice= {"white", "brown", "no rice", "all rice"};
+    String[] meat = {"chicken","steak","carnidas","chorizo","sofritas","veggies","no meat","all meat"};
+    String[] beans = {"pinto","black","no beans"};
+    String[] salsa ={"mild","medium","hot","no salsa","all salsa"};
+    String[] veggies = {"lettuce","fajitas veggies","no veggies","all veggies"};
+    boolean cont = true;
+    Random rand = new Random();
+    //int numOfBurr = 25;
+
+    while(cont) {
+        //fill in complete burrito array
+        // for (int i = 1; i < burritos.length; i++) {//for every burritos starting from 1
+        int randRice = rand.nextInt(rice.length);//random generator for rice
+        String r = (rice[randRice]);
+        completeBurr.add(r);//add to burrito
+
+        //System.out.println(r);
+
+        int randMeat = rand.nextInt(meat.length);//random generator for meat
+        String m = (meat[randMeat]);
+        completeBurr.add(m);
+        //System.out.println(m);
+
+        int randBeans = rand.nextInt(beans.length);//random generator for beans
+        String b = (beans[randBeans]);
+        completeBurr.add(b);
+
+        int randSalsa = rand.nextInt(salsa.length);//random generator for salsa
+        String s = (salsa[randSalsa]);
+        completeBurr.add(s);
+
+        int randVeg = rand.nextInt(veggies.length);//random generator for veggies
+        String v = (veggies[randVeg]);
+        completeBurr.add(v);
+
+        cont = false;
+        //System.out.println("After add ingredients: " + completeBurr);
+        //}//end for
+    }//end while
+    return completeBurr;
 }
 
 
